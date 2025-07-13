bbb705989ff9023c59d8483facb01b1e9b4edcfd
Oebelus 🐧 <imaneelfidrissi@hotmail.com>
2025-07-13 10:50:39 +0100

    adding more basic combinators (specific and general)

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # My parser combinator Library
+
+## 1. Basic Combinators
+
+### Specific Matchers
+
+They match a specific input:
+
+- `char` – Parses a single specific character;
+- `literal` – Parses a specific sequence of characters;
+
+### General Matchers
+
+They succeed on any valid input, rather than matching an exact expected value:
+
+- `digit` – Parses a single digit (0-9);
+- `number` – Parses many digits;
+- `letter` – Parses a single letter (a-z, A-Z);
+- `word` - Parses many letters;
+- `any_char` – Parses any single character;
+- `alpha_num` – Parses a single alphanumeric character (letter or digit);
+- `alpha_num_word` - Parses a sequence of alphanumeric characters;
+- `whitespace` – Parses a single whitespace character (space, tab, newline);
+- `spaces` – Parses zero or more whitespace characters;
+- `end_of_input` – Succeeds only if the end of the input is reached.
+
+## 2. Logical Combinators
+
+- `or` – Tries the first parser, and if it fails, tries the second (alternative);
+- `and` – Runs two parsers in sequence and combines their results;
+- `zero_or_more` – Parses zero or more occurrences of a given parser;
+- `one_or_more` – Parses one or more occurrences of a given parser;
+- `optional` – Parses zero or one occurrence of a given parser;
+- `sep_by` – Parses a sequence separated by a delimiter (e.g., `1,2,3` to `[1, 2, 3]`);
+- `sep_by_one` – Like `sep_by`, but requires at least one item, fails if no items are found;
+- `between` – Parsers something between two other parsers (e.g., brackets `(x)` to `x`).
+
+## 3. Transformation
+
+- `map` – Applies a function to the result of a parser;
+- `seq` – Chains parsers;
+- `look_ahead` – Checks if a parser matches without consuming input;
+- `not_followed_by` – The opposite of `look_ahead`, succeeds only if the given parser fails.
+
+## 4. Error Handling
+
+- `fail` – Always fails with a given error message:
+  - For custom validation logic (e.g., "number must be positive");
+  - As a placeholder for unimplemented parsers.
+- `expect` – Wraps another parser to improve its error message:
+  - When I know what syntax I should expect =  `expected "X" but found "Y"`.
+
+## 4. Utility Parsers
+
+- `identifier` – Parses a variable-like identifier (letters, digits, underscores).
+- `quoted_string` – Parses a string inside quotes (e.g., "hello").
+- `token` – Parses a value and consumes trailing whitespace (for lexing).
+- `chain_l` – Parses left-associative operator chains (e.g., 1 + 2 + 3).
+- `chain_r` – Parses right-associative operator chains (e.g., a = b = c).
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use crate::parser;
+
 pub type Parsed<'a, Output> = Result<(&'a str, Output), &'a str>; // Parser Result
 
 pub trait Parser<'a, Output> {
@@ -26,66 +28,112 @@ pub enum Types<'a> {
     StrVec(Vec<&'a str>),
 }
 
-pub fn match_literal<'a>(expected: &'a str) -> impl Parser<'a, ()> {
+/* BASIC COMBINATORS */
+
+// SPECIFIC MATCHERS
+pub fn char<'a>(expected: &'a char) -> impl Parser<'a, ()> {
+    move |input: &'a str| match input.chars().nth(0).unwrap() {
+        next if next == *expected => Ok((&input[1..], ())),
+        _ => Err(input),
+    }
+}
+
+pub fn literal<'a>(expected: &'a str) -> impl Parser<'a, ()> {
     move |input: &'a str| match input.get(0..expected.len()) {
         Some(next) if next == expected => Ok((&input[expected.len()..], ())),
         _ => Err(input),
     }
 }
 
-pub fn digits<'a>(expected: &'a str) -> impl Parser<'a, ()> {
-    move |input: &'a str| match input.get(0..expected.len()) {
-        Some(next) if next == expected && next.chars().all(|c| c.is_numeric()) => {
-            Ok((&input[expected.len()..], ()))
+// GENERAL MATCHERS
+pub fn any_char<'a>() -> impl Parser<'a, char> {
+    move |input: &'a str| match input.chars().next() {
+        Some(next) => Ok((&input[1..], next)),
+        _ => Err(input),
+    }
+}
+
+pub fn digit<'a>() -> impl Parser<'a, char> {
+    move |input: &'a str| match input.chars().next() {
+        Some(next) => {
+            if next.is_numeric() {
+                Ok((&input[1..], next))
+            } else {
+                Err(input)
+            }
         }
         _ => Err(input),
     }
 }
 
-pub fn number<'a>() -> impl Parser<'a, String> {
-    move |input: &'a str| {
-        let mut result = String::new();
+pub fn digits<'a>() -> impl Parser<'a, String> {
+    map(one_or_more(digit()), |chars| chars.iter().collect())
+}
 
-        for c in input.chars() {
-            if c.is_numeric() {
-                result.push(c);
+pub fn letter<'a>() -> impl Parser<'a, char> {
+    move |input: &'a str| match input.chars().next() {
+        Some(next) => {
+            if next.is_alphabetic() {
+                Ok((&input[1..], next))
             } else {
-                break;
+                Err(input)
             }
         }
+        _ => Err(input),
+    }
+}
 
-        let length = result.len();
+pub fn word<'a>() -> impl Parser<'a, String> {
+    map(one_or_more(letter()), |chars| chars.iter().collect())
+}
 
-        if length == 0 {
+pub fn alpha_num<'a>() -> impl Parser<'a, char> {
+    move |input: &'a str| match input.chars().next() {
+        Some(next) => {
+            if next.is_alphanumeric() {
+                Ok((&input[1..], next))
+            } else {
+                Err(input)
+            }
+        }
+        _ => Err(input),
+    }
+}
+
+pub fn alpha_num_word<'a>() -> impl Parser<'a, String> {
+    map(one_or_more(letter()), |chars| chars.iter().collect())
+}
+
+pub fn whitespace<'a>() -> impl Parser<'a, char> {
+    move |input: &'a str| match input.chars().next() {
+        Some(next) => {
+            if next == '\r' || next == ' ' || next == '\t' {
+                Ok((&input[1..], next))
+            } else {
+                Err(input)
+            }
+        }
+        _ => Err(input),
+    }
+}
+
+pub fn spaces<'a>() -> impl Parser<'a, String> {
+    map(zero_or_more(whitespace()), |chars| chars.iter().collect())
+}
+
+pub fn end_of_input<'a>() -> impl Parser<'a, ()> {
+    move |input: &'a str| {
+        if input.is_empty() {
+            Ok(("", ()))
+        } else {
             Err(input)
-        } else {
-            Ok((&input[length..], result))
         }
     }
 }
 
-pub fn identifier<'a>(input: &str) -> Parsed<String> {
-    let mut matched = String::new();
-    let mut chars = input.chars();
+/* LOGICAL COMBINATORS */
 
-    match chars.next() {
-        Some(next) if next.is_alphabetic() => matched.push(next),
-        _ => return Err(input),
-    }
-
-    while let Some(next) = chars.next() {
-        if next.is_alphanumeric() || next == '-' {
-            matched.push(next);
-        } else {
-            break;
-        }
-    }
-
-    let next_index = matched.len();
-    Ok((&input[next_index..], matched))
-}
-
-pub fn pair<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, (C, D)>
+pub fn and<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, (C, D)>
 where
     A: Parser<'a, C>,
     B: Parser<'a, D>,
@@ -99,31 +147,33 @@ where
     }
 }
 
-pub fn left<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, C>
+pub fn or<'a, P1, P2, A, B>(parser_a: P1, parser_b: P2) -> impl Parser<'a, Either<A, B>>
 where
-    A: Parser<'a, C>,
-    B: Parser<'a, D>,
+    P1: Parser<'a, A>,
+    P2: Parser<'a, B>,
 {
-    map(pair(parser_a, parser_b), |(left, _right)| left)
+    move |input| match parser_a.parse(input) {
+        Ok((next_input, result)) => Ok((next_input, Either::Left(result))),
+        Err(_) => match parser_b.parse(input) {
+            Ok((next_input, result)) => Ok((next_input, Either::Right(result))),
+            Err(e) => Err(e),
+        },
+    }
 }
 
-pub fn right<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, D>
-where
-    A: Parser<'a, C>,
-    B: Parser<'a, D>,
-{
-    map(pair(parser_a, parser_b), |(_left, right)| right)
-}
-
-pub fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+pub fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
 where
     P: Parser<'a, A>,
-    F: Fn(A) -> B,
 {
-    move |input| {
-        parser
-            .parse(input)
-            .map(|(next_input, result)| (next_input, map_fn(result)))
+    move |mut input| {
+        let mut result = Vec::new();
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
     }
 }
 
@@ -150,33 +200,31 @@ where
     }
 }
 
-pub fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+pub fn left<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, C>
 where
-    P: Parser<'a, A>,
+    A: Parser<'a, C>,
+    B: Parser<'a, D>,
 {
-    move |mut input| {
-        let mut result = Vec::new();
-
-        while let Ok((next_input, next_item)) = parser.parse(input) {
-            input = next_input;
-            result.push(next_item);
-        }
-
-        Ok((input, result))
-    }
+    map(and(parser_a, parser_b), |(left, _right)| left)
 }
 
-pub fn or<'a, P1, P2, A, B>(parser_a: P1, parser_b: P2) -> impl Parser<'a, Either<A, B>>
+pub fn right<'a, A, B, C, D>(parser_a: A, parser_b: B) -> impl Parser<'a, D>
 where
-    P1: Parser<'a, A>,
-    P2: Parser<'a, B>,
+    A: Parser<'a, C>,
+    B: Parser<'a, D>,
 {
-    move |input| match parser_a.parse(input) {
-        Ok((next_input, result)) => Ok((next_input, Either::Left(result))),
-        Err(_) => match parser_b.parse(input) {
-            Ok((next_input, result)) => Ok((next_input, Either::Right(result))),
-            Err(e) => Err(e),
-        },
+    map(and(parser_a, parser_b), |(_left, right)| right)
+}
+
+pub fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+where
+    P: Parser<'a, A>,
+    F: Fn(A) -> B,
+{
+    move |input| {
+        parser
+            .parse(input)
+            .map(|(next_input, result)| (next_input, map_fn(result)))
     }
 }
 
@@ -213,4 +261,26 @@ pub fn ends_with<'a>(suffix: &'a str) -> impl Parser<'a, bool> {
         true => Ok((input, true)),
         false => Err(input),
     }
+}
+
+/* UTILITY PARSERS */
+pub fn identifier<'a>(input: &str) -> Parsed<String> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
 }
